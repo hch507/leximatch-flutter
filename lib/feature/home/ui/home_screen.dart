9e7bfbed6b03f46c3f8ce8c45bbcbf4427b38640
Han Chunghee <96819679+hch507@users.noreply.github.com>
2026-04-23 10:34:54 +0900

    feat : home_screen 이미지 적용

--- lib/feature/home/ui/home_screen.dart
+++ lib/feature/home/ui/home_screen.dart
@@ -9,19 +9,88 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(body: HomeBody());
+  }
+}
+
+// 1. 별도의 클래스로 추출
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/leximatch_background.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: const _HomeContent(),
+    );
+  }
+}
+
+class _HomeContent extends StatelessWidget {
+  const _HomeContent();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
         children: [
-          Text("Home Screen"),
-          ElevatedButton(
-            onPressed: () {
-              print("게임으로 이동");
-              context.go(RoutePath.game);
-            },
-            child: const Text("게임으로 이동"),
-          )
+          // 🔥 상단 30% (로고 영역)
+          Expanded(
+            flex: 3,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Image.asset(
+                  'assets/images/leximatch_title_logo.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+
+          Expanded(
+            flex: 5,
+            child: SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Image.asset(
+                'assets/images/home_lotto_icon.png',
+                fit: BoxFit.contain, // ⭐️ 중요
+              ),
+            ),
+          ),
+          // 🔥 하단 70% (버튼 영역)
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => context.go(RoutePath.game),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 16,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    "게임 시작",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
